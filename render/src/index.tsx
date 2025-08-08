897f83834fa4df7fe19107d92b2f6ef999ae597b
Austin Chen <akrolsmir@gmail.com>
2025-08-07 17:41:13 -0700

    Try forcing dev mode for Render

--- render/src/index.tsx
+++ render/src/index.tsx
@@ -23,14 +23,7 @@ const server = serve({
     },
   },
 
-  // Seems like "development: true" is needed for bun build?
-  development: process.env.NODE_ENV !== 'production' && {
-    // Enable browser hot reloading in development
-    hmr: true,
-
-    // Echo console logs from the browser to the server
-    console: true,
-  },
+  development: true,
 })
 
 console.log(`🚀 Server running at ${server.url}`)
